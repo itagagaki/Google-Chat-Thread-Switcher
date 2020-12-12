d0ba3df47bcf1f89bb1fe37b9ff4b1b8d3531c63
Itagaki Fumihiko <itagagaki@gmail.com>
2020-12-12 22:07:17 +0900

    Refactoring
    Removed grouping function that were unnecessary and not working and refactored.

--- fixes/threads/thread-v2.js
+++ fixes/threads/thread-v2.js
@@ -61,42 +61,6 @@ const formatTitleFromThreadHeading = title => {
     //.replace(/【([^(【】)]+)】/g, '$1'); /* remove 【】 */
 };
 
-/* helper methods for group configs */
-const hasEmoji = emoji => thread => thread.threadHeading.includes(emoji);
-const updatedToday = thread => thread.threadHeading.match(/Last updated: \d+:\d+ (A|P)M/);
-const updatedNow = thread => thread.threadHeading.match(/Last updated: Now/);
-const addedNow = thread => thread.threadHeading.match(/\. Now$/);
-const addedNMinutesAgo = thread => thread.threadHeading.match(/(\.|\:) \d+ mins?$/);
-const updatedYesterday = thread => thread.threadHeading.match(/Last updated: Yesterday \d+:\d+ (A|P)M/);
-const fallback = () => true;
-
-/* default group config, you can set your own in localstorage if you want */
-const groupingConfigs = window.localStorage.getItem('thread-group-configs') || [
-  { label: 'Pinned', check: [hasEmoji('📌'), hasEmoji('⭐'), hasEmoji('🔔')] },
-  { label: 'Hidden', check: [hasEmoji('🔕'), hasEmoji('⛔'), hasEmoji('🚫')], shouldHide: true },
-  { label: 'Today', check: [updatedToday, updatedNow, addedNow, addedNMinutesAgo] }, /* updated today */
-  { label: 'Yesterday', check: [updatedYesterday] }, /* updated yesterday */
-  { label: 'Older', check: [fallback] }, /* updated before yesterday */
-];
-
-/* some very complicated logic for putting threads into groups */
-const groupThreads = (threadSelectors, configs) => {
-  /* add a threads field for each config (where we will put the relevant threads) */
-  const configsWithThreads = configs
-    .map(config => ({ ...config, threads: [] }));
-
-  /* for each thread, find the first config where some check passes */
-  /* and add the thread to that config */
-  return threadSelectors
-    .reduce((groupConfigs, thread) => {
-      const passingConfigIndex = groupConfigs.findIndex(config => config.check.some(check => check(thread)));
-      const passingConfig = groupConfigs[passingConfigIndex];
-      const updatedConfig = { ...passingConfig, threads: [...passingConfig.threads, thread] };
-      return groupConfigs
-        .map((config, configIndex) => configIndex === passingConfigIndex ? updatedConfig : config);
-    }, configsWithThreads);
-};
-
 /* build the switcher */
 const buildSwitcher = () => {
   /* query for threads in this room */
@@ -114,60 +78,43 @@ const buildSwitcher = () => {
     return null;
   }
 
-  /* build selectors (buttons) that will unhide the thread */
-  const threadSelectors = threadHeadersArray
+  /* build thread list (as <option>s for <select>) */
+  const threadList = threadHeadersArray
     .map((thread) => {
-      const newSelector = document.createElement('option');
-      newSelector.className = 'thread-list-item';
-      newSelector.threadHeading = thread.textContent;
+      const item = document.createElement('option');
+      item.className = 'thread-list-item';
+      item.threadHeading = thread.textContent;
       const titleArr = thread.textContent.split('.');
       if (titleArr[0].indexOf('Unread') >= 0 || titleArr[0].indexOf('未読') >= 0) {
-        newSelector.className = newSelector.className + ' thread-unread';
+        item.className = item.className + ' thread-unread';
         titleArr.shift();
       }
       titleArr.shift();
-      newSelector.innerText = formatTitleFromThreadHeading(titleArr.join('.'));
-      return newSelector;
+      item.innerText = formatTitleFromThreadHeading(titleArr.join('.'));
+      return item;
     });
 
-  /* create the switcher DOM */
-  const switcher = document.createElement('div');
-  switcher.id = 'thread-switcher';
+  /* build <select> DOM */
+  const selectDOM = document.createElement('select');
+  //selectDOM.id = `thread-group-${group.label}`;
+  selectDOM.id = 'thread-selector';
+  selectDOM.onchange = switchThread;
 
-  /* build groups based on checks in the group configs */
-  const groupings = groupThreads(threadSelectors, groupingConfigs);
+  const head = document.createElement('option');
+  head.hidden = true;
+  head.disabled = true;
+  head.className = 'thread-list-header';
+  head.innerText = 'スレッド移動';
+  selectDOM.appendChild(head);
+  threadList.forEach(option => selectDOM.appendChild(option));
 
-  /* build container DOM based on the groups */
-  const groupContainers = groupings
-    .filter(group => group.threads.length > 0) /* remove any groups where there are no threads */
-    .filter(group => !group.shouldHide) /* don't add any threads that should be hidden */
-    .map((group, groupIndex, unfilteredGroups) => {
-      /* build groups */
-      const groupDOM = document.createElement('select');
-      //groupDOM.id = `thread-group-${group.label}`;
-      groupDOM.id = 'thread-selector';
-      groupDOM.onchange = switchThread;
+  selectDOM.selectedIndex = 0;
 
-      const head = document.createElement('option');
-      head.hidden = true;
-      head.disabled = true;
-      head.className = 'thread-list-header';
-      head.innerText = 'スレッド移動';
-      groupDOM.appendChild(head);
-
-      /* append all the threads in the group */
-      group.threads
-        .forEach(thread => groupDOM.appendChild(thread));
-
-      groupDOM.selectedIndex = 0;
-      return groupDOM;
-    });
-
-  /* append each group container to the switcher */
-  groupContainers
-    .forEach(groupContainer => switcher.appendChild(groupContainer));
-
-  return switcher;
+  /* build the switcher DOM */
+  const switcherDOM = document.createElement('div');
+  switcherDOM.id = 'thread-switcher';
+  switcherDOM.appendChild(selectDOM);
+  return switcherDOM;
 };
 
 /* logic for building the switcher and injecting it into the page */
